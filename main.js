b1f66a4674764f3e12de70928d6293f59c66d5f0
Marcin Kozicki <m.kozicki2@gmail.com>
2019-11-12 21:33:44 +0100

    Add scroller another feature

--- main.js
+++ main.js
@@ -1,39 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('witaj')
+  const scroller = new Scroller('#root')
+  console.log(scroller);
 
-  const rootElement = document.querySelector('#root');
-  const sections = document.querySelectorAll('section');
-  let currentSectionIndex = 0;
-  let isThrottled = false;
 
-  document.addEventListener('mousewheel', function (event) {
-    if (isThrottled) return;
-    isThrottled = true
-    setTimeout(() => {
-      isThrottled = false
-    }, 1000);
-    const direction = event.wheelDelta < 0 ? 1 : -1;
+  document.addEventListener('mousewheel', scroller.listenScroll)
 
-    scroll(direction)
-
-  })
   function scroll(direction) {
-    if (direction === 1) {
-      const isLastSection = currentSectionIndex === sections.length - 1;
-      if (isLastSection) return
-    } else if (direction === -1) {
-      const isFirstSection = currentSectionIndex === 0;
-      if (isFirstSection) return;
-    }
-    currentSectionIndex += direction
 
-    scrollToCurrentSection()
   }
 
-  function scrollToCurrentSection() {
-    sections[currentSectionIndex].scrollIntoView({
-      behavior: "smooth",
-      block: 'start',
-    })
-  }
+
 })
