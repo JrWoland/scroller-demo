ac7b767fb0e434c6aed0eb0ddfacb737dbf5ef5a
Marcin Kozicki <m.kozicki2@gmail.com>
2019-11-12 18:27:42 +0100

    Code refactor

--- main.js
+++ main.js
@@ -9,12 +9,15 @@ document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('mousewheel', function (event) {
     if (isThrottled) return;
     isThrottled = true
-
     setTimeout(() => {
       isThrottled = false
     }, 1000);
-
     const direction = event.wheelDelta < 0 ? 1 : -1;
+
+    scroll(direction)
+
+  })
+  function scroll(direction) {
     if (direction === 1) {
       const isLastSection = currentSectionIndex === sections.length - 1;
       if (isLastSection) return
@@ -23,11 +26,14 @@ document.addEventListener('DOMContentLoaded', function () {
       if (isFirstSection) return;
     }
     currentSectionIndex += direction
-    console.log(event.wheelDelta)
 
+    scrollToCurrentSection()
+  }
+
+  function scrollToCurrentSection() {
     sections[currentSectionIndex].scrollIntoView({
       behavior: "smooth",
       block: 'start',
     })
-  })
+  }
 })
