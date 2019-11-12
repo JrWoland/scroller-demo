07bcd4ce02563b2ab671feaeaa28da7476e1f997
Marcin Kozicki <m.kozicki2@gmail.com>
2019-11-12 17:58:05 +0100

    Implement scroll feature

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
   console.log('witaj')
 
-  const rootElement = document.querySelector('#root')
-  const sections = document.querySelectorAll('section')
+  const rootElement = document.querySelector('#root');
+  const sections = document.querySelectorAll('section');
+  let currentSectionIndex = 0;
+  let isThrottled = false;
 
   document.addEventListener('mousewheel', function (event) {
-    console.log(event);
+    if (isThrottled) return;
+    isThrottled = true
 
+    setTimeout(() => {
+      isThrottled = false
+    }, 1000);
+
+    const direction = event.wheelDelta < 0 ? 1 : -1;
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return
+    } else if (direction === -1) {
+      const isFirstSection = currentSectionIndex === 0;
+      if (isFirstSection) return;
+    }
+    currentSectionIndex += direction
+    console.log(event.wheelDelta)
+
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: 'start',
+    })
   })
 })
